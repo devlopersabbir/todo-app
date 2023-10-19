846c294a9e2fe1d8b972aa56f9b7e3e02f000374
devlopersabbir <devlopersabbir@gmail.com>
2023-10-19 14:18:40 +0600

    project init

---
+++ client/app/page.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <section className="px-4 py-10 max-w-4xl mx-auto">
+      <h1 className="underline text-4xl text-center text-white">
+        Next13 TODO Application
+      </h1>
+      <button className="btn">Click me </button>
+    </section>
+  );
+};
+
+export default Home;
---
+++ client/constants/__data.ts
@@ -0,0 +1,12 @@
+export const __data = [
+  {
+    title: "have to read python book",
+    description: "I have to read more then 2 hours python books",
+    isFinished: false,
+  },
+  {
+    title: "have to read Java book",
+    description: "I have to read more then 2 hours Java books",
+    isFinished: true,
+  },
+];
