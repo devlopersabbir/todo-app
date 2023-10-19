ff9658b971a1af8ad6a393e8f6fb33ec5aab5fdd
devlopersabbir <devlopersabbir@gmail.com>
2023-10-19 21:55:58 +0600

    todo model created

--- server/src/models/todo.py
+++ server/src/models/todo.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy import Column, String, Boolean, Text
 from src.models.baseModels import BaseModels
 
 
 class Todo(BaseModels):
     __tablename__ = "Todos"
 
-    name = Column(String(256), unique=True)
+    name = Column(String(256), unique=True, nullable=False)
+    description = Column(String(Text))
     isDone = Column(Boolean(), default=False)
